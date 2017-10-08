7911140b1b9cc35840da822e3d38b650da0f7c45
vinay <dinodia.vinay@gmail.com>
2017-10-08 10:02:30 +0530

    Set all APIs

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 var express = require ("express");
 var app = express();
-var port =process.env.PORT || 3000;
+var port =process.env.PORT || 8000;
 var morgan =require('morgan');
 var mongoose = require('mongoose');
+var bodyParser = require ('body-parser');
+var router = express.Router();
+var appRoutes = require('./app/routes/api')(router);
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());//for parsing application/json
+app.use(bodyParser.urlencoded({extended: true}));//for parsing appliction/www-from-urlencoded
+app.use('/api',appRoutes);
+//http://localhost:8000/api/users
 mongoose.connect('mongodb://localhost:27017/tutorial',function(err){
 	if(err){
-	console.log('Not connected to the database:'+err);
+	console.log('Not connected to the database:'+ err);
 }else{
-	console.log('SuccessFully connected to MongoDB');
+	console.log('Successfully connected to MongoDB');
 }
 });
 
